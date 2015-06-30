2950f3ed799adabd3472d7119c08a7df23de2bbb
Samy Gejzenblozen <sam@iceman.local>
2015-06-30 22:10:55 +0200

    Tutorials ok, chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,13 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-    render 'new'
+      render 'new'
     end
   end
 
-  private
+## Secure parameter passing mode
+## ==============================
+private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
