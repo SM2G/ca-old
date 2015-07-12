c0b273dced38c01a5294a77e00db87825ee46993
Samy Gejzenblozen <sam@iceman.local>
2015-07-12 23:47:21 +0200

    cleaning and posting to Heroku

--- db/migrate/20150712172949_add_email_to_sessions.rb
+++
@@ -1,7 +0,0 @@
-class AddEmailToSessions < ActiveRecord::Migration
-  def change
-    add_column :sessions, :email, :string
-    add_column :sessions, :password, :string
-    add_column :sessions, :remember_me, :integer
-  end
-end
