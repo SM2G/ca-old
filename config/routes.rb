40e38cf1a04c035fbdcea8936113136e672bca11
Samy Gejzenblozen <sam@iceman.local>
2015-07-14 18:19:43 +0200

    thing seems to work

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,9 @@ Rails.application.routes.draw do
   resources :alerts
   resources :documents
   resources :employees
-  resources :filters
-  resources :filter_boxes
   resources :sessions
   resources :users
-  
+
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
