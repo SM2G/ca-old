deef03b44a1c6a67ea26867cf553bc4bdf1059e9
Samy Gejzenblozen <sam@iceman.local>
2015-07-15 15:55:31 +0200

    fixing tests in progress

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,10 +6,10 @@ test "invalid signup information" do
   get signup_path
   assert_no_difference 'User.count' do
     post users_path, user: { name:  "",
-                             email: "user@invalid",
-                             password: "foo",
-                             password_confirmation: "bar" }
-                           end
+    email: "user@invalid",
+    password: "foo",
+    password_confirmation: "bar" }
+    end
     assert_template 'users/new'
     assert is_logged_in?
   end
