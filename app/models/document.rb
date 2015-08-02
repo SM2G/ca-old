4a14102edd049dfc2130637c3398dc1c75756d7c
Samy Gejzenblozen <sam@iceman.local>
2015-08-02 23:23:33 +0200

    dates ok, barres de statut pas encore ok.

--- app/models/document.rb
+++ app/models/document.rb
@@ -6,25 +6,16 @@ has_many :assignments
 has_many :profiles, through: :assignments
 has_many :paper
 
-  
+
 ## Structure
 ## ==============================
 validates :document_name,
     presence: true,
     length: { minimum: 2, maximum: 50 }
-validates :warning_months,
-    presence: true,
-    numericality: true
   validates :warning_days,
     presence: true,
     numericality: true
-  validates :critical_months,
-    presence: true,
-    numericality: true
   validates :critical_days,
-    presence: true,
-    numericality: true
-validates :expire_months,
     presence: true,
     numericality: true
   validates :expire_days,
