445effef1b5dd9e4afa2df8c539d7a8ea47dc0a7
Cecile Veneziani <contact@cecilitse.org>
2015-11-17 00:04:27 +0100

    Fix locales
    - Add FR locales for models
    - Remove useless EN locales

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,8 @@ module CerbereApp
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :fr
+    config.i18n.load_path      += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale  = :fr
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
