4f49b657b45b08ccf90bf8489cbc84c3c6c949df
Cecile Veneziani <contact@cecilitse.org>
2015-11-16 23:26:05 +0100

    Turn app into fr default locale

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module CerbereApp
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :fr
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
@@ -28,6 +28,5 @@ module CerbereApp
     config.serve_static_files = true
     config.assets.initialize_on_precompile = false
     config.assets.js_compressor = :uglifier
-
   end
 end
